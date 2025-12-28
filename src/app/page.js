a22c3606e8f45a8a81cdf659b89891416f58ebd5
subinseo-research <stm01@naver.com>
2025-12-27 21:54:34 -0500

    deploy error modify

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { WelcomePage } from "@/welcome/page";
+import { WelcomePage } from "../welcome/page";
 
 export default function HomePage() {
   const router = useRouter();
